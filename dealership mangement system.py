41910258ce552d14b38b3d29e79556e8f21a68a6
Ahmed <ahmedelshekih@gmail.com>
2024-12-09 18:22:40 +0200

    our dealership now sell bikes

--- dealership mangement system.py
+++ dealership mangement system.py
@@ -4,9 +4,9 @@ import time
 
 def main_menu():
     print("Welcome to the Dealership Management System!")
-    print("1. Add a new car")
-    print("2. Display available cars")
-    print("3. Buy a car")
+    print("1. Add a new vehicle")
+    print("2. Display available vehicles")
+    print("3. Buy a vehicle")
     print("4. Display customer purchases")
     print("5. Exit")
 
@@ -24,8 +24,13 @@ def main():
         
         if choice == "1":
             clear_screen()
-            Cars.add_car()
-            print("Car added successfully!")
+            addChoice = input("[1] Car \n[2] Bike \nWhat do you want to add : ")
+            if addChoice == "1":
+                Cars.add_car()
+                print("Car added successfully!")
+            elif addChoice == "2":
+                Bikes.add_bike()
+                print("Bike added successfully!")
             input("Press Enter to return to the menu.")
         
         elif choice == "2":
@@ -43,7 +48,13 @@ def main():
                 customers[name] = Customer(name, phone)
             
             customer = customers[name]
-            Cars.display_available_cars()
+
+            print("[1] car \n[2] Bike")
+            vehicleChoice = input("What do you want to add : ")
+            if vehicleChoice =="1":
+                Cars.display_available_cars()
+            elif vehicleChoice =="2":
+                Bikes.display_available_bikes()
             
             try:
                 car_index = int(input("Enter the number of the car to buy : ")) - 1
--- dealership_objects.py
+++ dealership_objects.py
@@ -1,16 +1,47 @@
-class Cars:
+class Vehicle:
+    def __init__(self, brand, model, year, price, isAvailable):
+        self.brand = brand 
+        self.model = model
+        self.year = year
+        self.price = price
+        self.isAvailable = isAvailable
+
+    def get_info(self):
+        print(f"Vehicle name: {self.brand} {self.model}")
+        print(f"Year: {self.year}")
+        print(f"Price: ${self.price}")
+        print("Available for purchase" if self.isAvailable else "Sold")
+        print("-" * 30)
+
+    def buy(self):
+        if self.isAvailable:
+            print(f"You have bought {self.brand} {self.model}")
+            self.isAvailable = False
+        else:
+            print(f"{self.brand} {self.model} is not available for purchase")
+
+    def return_(self):
+        print(f"you have returned {self.brand} {self.model}")
+        Cars.cars_num += 1
+        self.isAvailable = False
+
+    @staticmethod
+    def display_available_vehicles(vehicle_list, vehicle_type="Vehicles"):
+        print(f"Available {vehicle_type}:")
+        if not vehicle_list:
+            print(f"No {vehicle_type.lower()} available.")
+            return
+        for vehicle in vehicle_list:
+            vehicle.get_info()
+
+class Cars(Vehicle):
 
     cars_num = 0
     available_cars = []
 
     def __init__(self, brand, model, year, form, price, isAvailable):
-        self.brand = brand 
-        self.model = model
-        self.year = year
+        super().__init__(brand, model, year, price , isAvailable)
         self.form = form
-        self.price = price
-        self.isAvailable = isAvailable
-
         Cars.cars_num += 1 
         Cars.available_cars.append(self)
         
@@ -24,13 +55,6 @@ class Cars:
         
         Cars(brand, model, year, form, price, True)
 
-    def get_info(self):
-        print(f"Car name: {self.brand} {self.model}")
-        print(f"Year: {self.year}")
-        print(f"Form: {self.form}")
-        print(f"Price: ${self.price}")
-        print("Available for purchase" if self.isAvailable else "Sold")
-        print("-" * 30)
 
     @staticmethod
     def display_available_cars():
@@ -42,20 +66,6 @@ class Cars:
         for car in Cars.available_cars:
             car.get_info()
 
-    def buy(self):
-        if self.isAvailable:
-            print(f"you have bought {self.brand} {self.model}")
-            Cars.cars_num -= 1 
-            self.isAvailable = False
-            Cars.available_cars.remove(self)
-        else:
-            print(f"{self.brand} {self.model} is not available for purchase")
-
-
-    def return_(self):
-        print(f"you have returned {self.brand} {self.model}")
-        Cars.cars_num += 1
-        self.isAvailable == False
 
     @staticmethod
     def cars_number():
@@ -65,6 +75,47 @@ class Cars:
             print(f"We have {Cars.cars_num} cars available")
             
 
+class Bikes(Vehicle):
+
+    bikes_num = 0
+    available_bikes = []
+
+    def __init__(self, brand, model, year, form, price, isAvailable):
+        super().__init__(brand, model, year, price , isAvailable)
+        self.form = form
+        Bikes.bikes_num += 1 
+        Bikes.available_bikes.append(self)
+        
+    @staticmethod
+    def add_bike():
+        brand = input("Enter brand: ")
+        model = input("Enter model: ")
+        year = input("Enter year: ")
+        form = input("Enter form: ")
+        price = float(input("Enter price: "))
+        
+        Bikes(brand, model, year, form, price, True)
+
+
+    @staticmethod
+    def display_available_bikes():
+        print("Available Bikes:")
+
+        if not Bikes.available_bikes:
+            print("No bikes available.")
+
+        for bike in Bikes.available_bikes:
+            bike.get_info()
+
+    
+    @staticmethod
+    def bikes_number():
+        if Bikes.bikes_num == 1:
+            print(f"We have 1 bike available") #يا تفاصيلك يا روكستار :)
+        else:
+            print(f"We have {Bikes.bikes_num} bikes available")
+
+
 class Customer:
 
 
@@ -73,18 +124,18 @@ class Customer:
         self.phone = phone
         self.purchased_vehicles = []
 
-    def add_purchase(self, car):
-        if car.isAvailable:
-            car.buy()
-            self.purchased_vehicles.append(car)
+    def add_purchase(self, vehicle):
+        if vehicle.isAvailable:
+            vehicle.buy()
+            self.purchased_vehicles.append(vehicle)
         else:
-            print(f"{car.brand} {car.model} is not available for purchase")
+            print(f"{vehicle.brand} {vehicle.model} is not available for purchase")
 
     def display_purchases(self):
         if self.purchased_vehicles:
             print(f"{self.name} has purchased the following vehicles:")
-            for car in self.purchased_vehicles:
-                print(f"- {car.brand} {car.model}")
+            for vehicle in self.purchased_vehicles:
+                print(f"- {vehicle.brand} {vehicle.model}")
         else:
             print(f"{self.name} has not purchased any vehicles.")
 
